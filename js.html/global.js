b2847c7e99c964f71983c3b6556f8f4f52de2752
parameshwarreddy614 <parameshwarreddy614@gmail.com>
2021-01-04 12:34:21 +0530

    added forms

---
+++ js.html/global.js
@@ -0,0 +1,10 @@
+var a=100;
+var h=60;
+
+function f1(){
+    console.log("the value of a"+a);
+}
+function f2(){
+    console.log("the value of h"+h);
+}
+console.log  
---
+++ js.html/variables.js
@@ -0,0 +1,8 @@
+ function square(){
+
+     var a=10;
+
+     console.log(a*a);
+
+ }
+ square();
